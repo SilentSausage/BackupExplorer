a6ee8eeca0214e05d746d2264cfdc1afbd8eca86
SilentSausage <39729679+SilentSausage@users.noreply.github.com>
2023-08-21 12:10:09 +1000

    Add files via upload

---
+++ admin/toolTipData.js
@@ -0,0 +1,152 @@
+var FiltersEnabled = 0; // if your not going to use transitions or filters in any of the tips set this to 0
+var spacer="&nbsp; &nbsp; &nbsp; ";
+
+// email notifications to admin
+notifyAdminNewMembers0Tip=["", spacer+"No email notifications to admin."];
+notifyAdminNewMembers1Tip=["", spacer+"Notify admin only when a new member is waiting for approval."];
+notifyAdminNewMembers2Tip=["", spacer+"Notify admin for all new sign-ups."];
+
+// visitorSignup
+visitorSignup0Tip=["", spacer+"If this option is selected, visitors will not be able to join this group unless the admin manually moves them to this group from the admin area."];
+visitorSignup1Tip=["", spacer+"If this option is selected, visitors can join this group but will not be able to sign in unless the admin approves them from the admin area."];
+visitorSignup2Tip=["", spacer+"If this option is selected, visitors can join this group and will be able to sign in instantly with no need for admin approval."];
+
+// Clients table
+Clients_addTip=["",spacer+"This option allows all members of the group to add records to the 'Clients' table. A member who adds a record to the table becomes the 'owner' of that record."];
+
+Clients_view0Tip=["",spacer+"This option prohibits all members of the group from viewing any record in the 'Clients' table."];
+Clients_view1Tip=["",spacer+"This option allows each member of the group to view only his own records in the 'Clients' table."];
+Clients_view2Tip=["",spacer+"This option allows each member of the group to view any record owned by any member of the group in the 'Clients' table."];
+Clients_view3Tip=["",spacer+"This option allows each member of the group to view all records in the 'Clients' table."];
+
+Clients_edit0Tip=["",spacer+"This option prohibits all members of the group from modifying any record in the 'Clients' table."];
+Clients_edit1Tip=["",spacer+"This option allows each member of the group to edit only his own records in the 'Clients' table."];
+Clients_edit2Tip=["",spacer+"This option allows each member of the group to edit any record owned by any member of the group in the 'Clients' table."];
+Clients_edit3Tip=["",spacer+"This option allows each member of the group to edit any records in the 'Clients' table, regardless of their owner."];
+
+Clients_delete0Tip=["",spacer+"This option prohibits all members of the group from deleting any record in the 'Clients' table."];
+Clients_delete1Tip=["",spacer+"This option allows each member of the group to delete only his own records in the 'Clients' table."];
+Clients_delete2Tip=["",spacer+"This option allows each member of the group to delete any record owned by any member of the group in the 'Clients' table."];
+Clients_delete3Tip=["",spacer+"This option allows each member of the group to delete any records in the 'Clients' table."];
+
+// Endpoints table
+Endpoints_addTip=["",spacer+"This option allows all members of the group to add records to the 'Endpoints' table. A member who adds a record to the table becomes the 'owner' of that record."];
+
+Endpoints_view0Tip=["",spacer+"This option prohibits all members of the group from viewing any record in the 'Endpoints' table."];
+Endpoints_view1Tip=["",spacer+"This option allows each member of the group to view only his own records in the 'Endpoints' table."];
+Endpoints_view2Tip=["",spacer+"This option allows each member of the group to view any record owned by any member of the group in the 'Endpoints' table."];
+Endpoints_view3Tip=["",spacer+"This option allows each member of the group to view all records in the 'Endpoints' table."];
+
+Endpoints_edit0Tip=["",spacer+"This option prohibits all members of the group from modifying any record in the 'Endpoints' table."];
+Endpoints_edit1Tip=["",spacer+"This option allows each member of the group to edit only his own records in the 'Endpoints' table."];
+Endpoints_edit2Tip=["",spacer+"This option allows each member of the group to edit any record owned by any member of the group in the 'Endpoints' table."];
+Endpoints_edit3Tip=["",spacer+"This option allows each member of the group to edit any records in the 'Endpoints' table, regardless of their owner."];
+
+Endpoints_delete0Tip=["",spacer+"This option prohibits all members of the group from deleting any record in the 'Endpoints' table."];
+Endpoints_delete1Tip=["",spacer+"This option allows each member of the group to delete only his own records in the 'Endpoints' table."];
+Endpoints_delete2Tip=["",spacer+"This option allows each member of the group to delete any record owned by any member of the group in the 'Endpoints' table."];
+Endpoints_delete3Tip=["",spacer+"This option allows each member of the group to delete any records in the 'Endpoints' table."];
+
+// Jobs table
+Jobs_addTip=["",spacer+"This option allows all members of the group to add records to the 'Jobs' table. A member who adds a record to the table becomes the 'owner' of that record."];
+
+Jobs_view0Tip=["",spacer+"This option prohibits all members of the group from viewing any record in the 'Jobs' table."];
+Jobs_view1Tip=["",spacer+"This option allows each member of the group to view only his own records in the 'Jobs' table."];
+Jobs_view2Tip=["",spacer+"This option allows each member of the group to view any record owned by any member of the group in the 'Jobs' table."];
+Jobs_view3Tip=["",spacer+"This option allows each member of the group to view all records in the 'Jobs' table."];
+
+Jobs_edit0Tip=["",spacer+"This option prohibits all members of the group from modifying any record in the 'Jobs' table."];
+Jobs_edit1Tip=["",spacer+"This option allows each member of the group to edit only his own records in the 'Jobs' table."];
+Jobs_edit2Tip=["",spacer+"This option allows each member of the group to edit any record owned by any member of the group in the 'Jobs' table."];
+Jobs_edit3Tip=["",spacer+"This option allows each member of the group to edit any records in the 'Jobs' table, regardless of their owner."];
+
+Jobs_delete0Tip=["",spacer+"This option prohibits all members of the group from deleting any record in the 'Jobs' table."];
+Jobs_delete1Tip=["",spacer+"This option allows each member of the group to delete only his own records in the 'Jobs' table."];
+Jobs_delete2Tip=["",spacer+"This option allows each member of the group to delete any record owned by any member of the group in the 'Jobs' table."];
+Jobs_delete3Tip=["",spacer+"This option allows each member of the group to delete any records in the 'Jobs' table."];
+
+// Log table
+Log_addTip=["",spacer+"This option allows all members of the group to add records to the 'Log' table. A member who adds a record to the table becomes the 'owner' of that record."];
+
+Log_view0Tip=["",spacer+"This option prohibits all members of the group from viewing any record in the 'Log' table."];
+Log_view1Tip=["",spacer+"This option allows each member of the group to view only his own records in the 'Log' table."];
+Log_view2Tip=["",spacer+"This option allows each member of the group to view any record owned by any member of the group in the 'Log' table."];
+Log_view3Tip=["",spacer+"This option allows each member of the group to view all records in the 'Log' table."];
+
+Log_edit0Tip=["",spacer+"This option prohibits all members of the group from modifying any record in the 'Log' table."];
+Log_edit1Tip=["",spacer+"This option allows each member of the group to edit only his own records in the 'Log' table."];
+Log_edit2Tip=["",spacer+"This option allows each member of the group to edit any record owned by any member of the group in the 'Log' table."];
+Log_edit3Tip=["",spacer+"This option allows each member of the group to edit any records in the 'Log' table, regardless of their owner."];
+
+Log_delete0Tip=["",spacer+"This option prohibits all members of the group from deleting any record in the 'Log' table."];
+Log_delete1Tip=["",spacer+"This option allows each member of the group to delete only his own records in the 'Log' table."];
+Log_delete2Tip=["",spacer+"This option allows each member of the group to delete any record owned by any member of the group in the 'Log' table."];
+Log_delete3Tip=["",spacer+"This option allows each member of the group to delete any records in the 'Log' table."];
+
+// Methods table
+Methods_addTip=["",spacer+"This option allows all members of the group to add records to the 'Methods' table. A member who adds a record to the table becomes the 'owner' of that record."];
+
+Methods_view0Tip=["",spacer+"This option prohibits all members of the group from viewing any record in the 'Methods' table."];
+Methods_view1Tip=["",spacer+"This option allows each member of the group to view only his own records in the 'Methods' table."];
+Methods_view2Tip=["",spacer+"This option allows each member of the group to view any record owned by any member of the group in the 'Methods' table."];
+Methods_view3Tip=["",spacer+"This option allows each member of the group to view all records in the 'Methods' table."];
+
+Methods_edit0Tip=["",spacer+"This option prohibits all members of the group from modifying any record in the 'Methods' table."];
+Methods_edit1Tip=["",spacer+"This option allows each member of the group to edit only his own records in the 'Methods' table."];
+Methods_edit2Tip=["",spacer+"This option allows each member of the group to edit any record owned by any member of the group in the 'Methods' table."];
+Methods_edit3Tip=["",spacer+"This option allows each member of the group to edit any records in the 'Methods' table, regardless of their owner."];
+
+Methods_delete0Tip=["",spacer+"This option prohibits all members of the group from deleting any record in the 'Methods' table."];
+Methods_delete1Tip=["",spacer+"This option allows each member of the group to delete only his own records in the 'Methods' table."];
+Methods_delete2Tip=["",spacer+"This option allows each member of the group to delete any record owned by any member of the group in the 'Methods' table."];
+Methods_delete3Tip=["",spacer+"This option allows each member of the group to delete any records in the 'Methods' table."];
+
+// Software table
+Software_addTip=["",spacer+"This option allows all members of the group to add records to the 'Software' table. A member who adds a record to the table becomes the 'owner' of that record."];
+
+Software_view0Tip=["",spacer+"This option prohibits all members of the group from viewing any record in the 'Software' table."];
+Software_view1Tip=["",spacer+"This option allows each member of the group to view only his own records in the 'Software' table."];
+Software_view2Tip=["",spacer+"This option allows each member of the group to view any record owned by any member of the group in the 'Software' table."];
+Software_view3Tip=["",spacer+"This option allows each member of the group to view all records in the 'Software' table."];
+
+Software_edit0Tip=["",spacer+"This option prohibits all members of the group from modifying any record in the 'Software' table."];
+Software_edit1Tip=["",spacer+"This option allows each member of the group to edit only his own records in the 'Software' table."];
+Software_edit2Tip=["",spacer+"This option allows each member of the group to edit any record owned by any member of the group in the 'Software' table."];
+Software_edit3Tip=["",spacer+"This option allows each member of the group to edit any records in the 'Software' table, regardless of their owner."];
+
+Software_delete0Tip=["",spacer+"This option prohibits all members of the group from deleting any record in the 'Software' table."];
+Software_delete1Tip=["",spacer+"This option allows each member of the group to delete only his own records in the 'Software' table."];
+Software_delete2Tip=["",spacer+"This option allows each member of the group to delete any record owned by any member of the group in the 'Software' table."];
+Software_delete3Tip=["",spacer+"This option allows each member of the group to delete any records in the 'Software' table."];
+
+// Destinations table
+Destinations_addTip=["",spacer+"This option allows all members of the group to add records to the 'Destinations' table. A member who adds a record to the table becomes the 'owner' of that record."];
+
+Destinations_view0Tip=["",spacer+"This option prohibits all members of the group from viewing any record in the 'Destinations' table."];
+Destinations_view1Tip=["",spacer+"This option allows each member of the group to view only his own records in the 'Destinations' table."];
+Destinations_view2Tip=["",spacer+"This option allows each member of the group to view any record owned by any member of the group in the 'Destinations' table."];
+Destinations_view3Tip=["",spacer+"This option allows each member of the group to view all records in the 'Destinations' table."];
+
+Destinations_edit0Tip=["",spacer+"This option prohibits all members of the group from modifying any record in the 'Destinations' table."];
+Destinations_edit1Tip=["",spacer+"This option allows each member of the group to edit only his own records in the 'Destinations' table."];
+Destinations_edit2Tip=["",spacer+"This option allows each member of the group to edit any record owned by any member of the group in the 'Destinations' table."];
+Destinations_edit3Tip=["",spacer+"This option allows each member of the group to edit any records in the 'Destinations' table, regardless of their owner."];
+
+Destinations_delete0Tip=["",spacer+"This option prohibits all members of the group from deleting any record in the 'Destinations' table."];
+Destinations_delete1Tip=["",spacer+"This option allows each member of the group to delete only his own records in the 'Destinations' table."];
+Destinations_delete2Tip=["",spacer+"This option allows each member of the group to delete any record owned by any member of the group in the 'Destinations' table."];
+Destinations_delete3Tip=["",spacer+"This option allows each member of the group to delete any records in the 'Destinations' table."];
+
+/*
+	Style syntax:
+	-------------
+	[TitleColor,TextColor,TitleBgColor,TextBgColor,TitleBgImag,TextBgImag,TitleTextAlign,
+	TextTextAlign,TitleFontFace,TextFontFace, TipPosition, StickyStyle, TitleFontSize,
+	TextFontSize, Width, Height, BorderSize, PadTextArea, CoordinateX , CoordinateY,
+	TransitionNumber, TransitionDuration, TransparencyLevel ,ShadowType, ShadowColor]
+
+*/
+
+toolTipStyle=["white","#00008B","#000099","#E6E6FA","","images/helpBg.gif","","","","\"Trebuchet MS\", sans-serif","","","","3",400,"",1,2,10,10,51,1,0,"",""];
+
+applyCssFilter();
